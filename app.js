ae6b1dc0b02a1553297889fc2e8c2b11b416a2d4
samirazazy <samiralazazy@gmail.com>
2020-07-03 17:30:50 +0200

    add  succsess and failure page

--- app.js
+++ app.js
@@ -32,14 +32,20 @@ app.post("/", function (req, res) {
 
   const jsonData = JSON.stringify(data);
 
-  const url = "https://us10.api.mailchimp.com/3.0/lists/a54ff13c1a";
+  const url = "https://us10.api.mailchimp.com/3.0/lists/f48b231267";
 
   const options = {
     method: "POST",
-    auth: "azazy1:2ea048743e2b14abb020f60c6826bb36-us10",
+    auth: "azazy1:e73edc1c8d9ac40f6e17c2eb12ab3d75-us10",
   };
 
   const request = https.request(url, options, function (response) {
+    if (response.statusCode == 200) {
+      res.sendFile(__dirname + "/success.html");
+    } else {
+      res.sendFile(__dirname + "/failure.html");
+    }
+
     response.on("data", function (data) {
       console.log(JSON.parse(data));
     });
@@ -47,6 +53,10 @@ app.post("/", function (req, res) {
 
   request.write(jsonData);
   request.end();
+});
+
+app.post("/failure", function (req, res) {
+  res.redirect("/");
 });
 
 app.listen(3000, function () {
