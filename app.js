440bf4907610147037246c8a50fb3a88c475a6c2
samirazazy <samiralazazy@gmail.com>
2020-07-03 15:01:22 +0200

    postinh form data to the root

--- app.js
+++ app.js
@@ -3,6 +3,21 @@ const bodyParser = require("body-parser");
 const request = require("request");
 
 const app = express();
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/signup.html");
+});
+
+app.post("/", function (req, res) {
+  firstName = req.body.fName;
+  firstName = req.body.lName;
+  email = req.body.email;
+  console.log(firstName, firstName, email);
+});
+
 app.listen(3000, function () {
   console.log("server started");
 });
